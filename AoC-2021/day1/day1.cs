5aa2a7d4b62c18aea570fffc67220f1cd033beb5
George Seeger <georgeseeger@hotmail.com>
2021-12-01 10:44:11 +0000

    Refactor day1

--- AoC-2021/day1/day1.cs
+++ AoC-2021/day1/day1.cs
@@ -1,6 +1,5 @@
 using System.IO;
 using System.Linq;
-using System.Net.Sockets;
 
 namespace AoC_2021 {
 
@@ -8,18 +7,18 @@ namespace AoC_2021 {
         public string Name => "Day1";
 
         public string Part1() {
-            var input = File.ReadAllLines("./day1/input.txt").Select((line) => int.Parse(line)).ToArray();
+            var input = File.ReadAllLines("./day1/input.txt").Select(int.Parse).ToArray();
 
-            var answer = input.Where((num, index) => index == 0 ? false : num > input[index - 1]).Count();
+            var answer = input.Skip(1).Where((num, index) => num > input[index]).Count();
 
             return answer.ToString();
         }
 
         public string Part2() {
-            var input = File.ReadAllLines("./day1/input.txt").Select((line) => int.Parse(line)).ToArray();
+            var input = File.ReadAllLines("./day1/input.txt").Select(int.Parse).ToArray();
 
-            var slidingWindowInput = input.Skip(2).Select((num, index) => input.Skip(index).Take(3).Sum()).ToArray();
-            var answer = slidingWindowInput.Where((num, index) => index == 0 ? false : num > slidingWindowInput[index - 1]).Count();
+            var sumOfPreviousThree = input.Skip(2).Select((num, index) => input.Skip(index).Take(3).Sum()).ToArray();
+            var answer = sumOfPreviousThree.Skip(1).Where((num, index) => num > sumOfPreviousThree[index]).Count();
 
             return answer.ToString();
         }
