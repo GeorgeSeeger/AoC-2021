41e257b227f1eab9846d7f015cc3adee5183aa1a
George Seeger <georgeseeger@hotmail.com>
2021-12-07 10:39:06 +0000

    Day 7 pow pow

---
+++ AoC-2021/day7/Day7.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC_2021 {
+    public class Day7 : IProblem {
+        public string Name => "Day7";
+
+        public string Part1() {
+            var input = File.ReadAllLines("./day7/input.txt").First().Split(",").Select(int.Parse).ToArray();
+
+            var fuelCosts = input.Select((_, dest) => input.Select(pos => Math.Abs(dest - pos)).Sum());
+
+            return fuelCosts.Min().ToString();
+        }
+
+        public string Part2() {
+            var input = File.ReadAllLines("./day7/input.txt").First().Split(",").Select(int.Parse).ToArray();
+            var fuelCosts = input.Select((_, dest) => input.Select(pos => {
+                var distance = Math.Abs(dest - pos);
+                return (int)((distance + 1) * (distance / 2M));
+            }).Sum());
+
+            return fuelCosts.Min().ToString();
+        }
+    }
+}
